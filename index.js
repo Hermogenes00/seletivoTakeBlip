3a0fc739373035fec1f2e10a444942b9ec3d6494
unknown <neto.programer@gmail.com>
2021-04-11 09:47:08 -0300

    env.port | 3000

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.json({})
 })
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Rodando na porta ${process.env.PORT}`)
 })
